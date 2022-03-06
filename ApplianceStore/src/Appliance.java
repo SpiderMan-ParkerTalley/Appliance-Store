2c51391283b968e982a61d2b0fe5091ca91ffbf4
Cristian Zendejas <48038935+CZend371@users.noreply.github.com>
2022-03-06 17:24:19 -0600

    The initial appliance class

---
+++ ApplianceStore/src/Appliance.java
@@ -0,0 +1,106 @@
+import java.sql.Timestamp;
+
+public class Appliance {
+	private int applianceId;
+	private String brandName;
+	private String model;
+	private double price;
+	private int quantity;
+	private Timestamp createdAt;
+	private boolean hasRepairPlan;
+	private int idCounter = 0;
+	
+	public Appliance(String brandName, String model, double price, int quantity, Timestamp createdAt,
+			boolean hasRepairPlan) {
+		this.applianceId = createId();
+		this.brandName = brandName;
+		this.model = model;
+		this.price = price;
+		this.quantity = quantity;
+		this.createdAt = createdAt;
+		this.createdAt = new Timestamp(System.currentTimeMillis());
+		this.hasRepairPlan = hasRepairPlan;
+	}
+
+	private int createId() {
+		idCounter++;
+		return idCounter;
+	}
+
+	public int getApplianceId() {
+		return applianceId;
+	}
+
+	public void setApplianceId(int applianceId) {
+		this.applianceId = applianceId;
+	}
+
+	public String getBrandName() {
+		return brandName;
+	}
+
+	public void setBrandName(String brandName) {
+		this.brandName = brandName;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public Timestamp getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Timestamp createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public boolean isHasRepairPlan() {
+		return hasRepairPlan;
+	}
+
+	public void setHasRepairPlan(boolean hasRepairPlan) {
+		this.hasRepairPlan = hasRepairPlan;
+	}
+	
+	/*
+	 * Returns the amount of appliances purchased and which ones were backorders?
+	 * @params quantity of desired appliances
+	 * @returns integer 
+	 */
+	public int purchase(int quantity) {
+		System.out.println("the purchase function runs!");
+		return 0;
+	}
+	
+	/*
+	 * Returns a boolean if the appliance is eligible for a repair plan
+	 * @params none
+	 * @returns boolean
+	 */
+	public boolean eligibleRepairPlan() {
+		System.out.println("the check for the repair plan works!");
+		return false;
+	}
+	
+}
