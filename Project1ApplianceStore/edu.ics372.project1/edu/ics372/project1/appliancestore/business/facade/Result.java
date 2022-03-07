65baaecd8a821e177554f4cfaa3fef73033c3623
Jim Sawicki <jamesvsawicki@gmail.com>
2022-03-07 12:06:55 -0600

    static Result codes are now Public

--- Project1ApplianceStore/edu.ics372.project1/edu/ics372/project1/appliancestore/business/facade/Result.java
+++ Project1ApplianceStore/edu.ics372.project1/edu/ics372/project1/appliancestore/business/facade/Result.java
@@ -8,14 +8,14 @@ package edu.ics372.project1.appliancestore.business.facade;
  */
 public class Result extends DataTransfer {
 
-	private static final int APPLIANCE_NOT_FOUND = 1;
-	private static final int CUSTOMER_NOT_FOUND = 2;
-	private static final int BACK_ORDER_NOT_FOUND = 3;
-	private static final int OPERATION_SUCCESSFUL = 4;
-	private static final int OPERATION_FAILED = 5;
-    private static final int BACKORDER_CREATED = 6;
+	public static final int APPLIANCE_NOT_FOUND = 1;
+	public static final int CUSTOMER_NOT_FOUND = 2;
+	public static final int BACK_ORDER_NOT_FOUND = 3;
+	public static final int OPERATION_SUCCESSFUL = 4;
+	public static final int OPERATION_FAILED = 5;
+    public static final int BACKORDER_CREATED = 6;
 
-	private int resultCode;
+	public int resultCode;
 
 	public int getResultCode() {
 		return resultCode;
