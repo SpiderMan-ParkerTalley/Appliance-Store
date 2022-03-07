e47f6769f10759ee50752a80fdbe6df23f2b57bb
Parker Talley <ptmn9278@gmail.com>
2022-03-07 14:22:59 -0600

    Added methods and fields to backorder and backorderlist

--- Project1ApplianceStore/edu.ics372.project1/edu/ics372/project1/appliancestore/business/collections/BackOrderList.java
+++ Project1ApplianceStore/edu.ics372.project1/edu/ics372/project1/appliancestore/business/collections/BackOrderList.java
@@ -1,5 +1,49 @@
 package edu.ics372.project1.appliancestore.business.collections;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+import edu.ics372.project1.appliancestore.business.entities.BackOrder;
+
 public class BackOrderList {
-    // Adding a secert comment
+    private List<BackOrder> backOrders = new LinkedList<BackOrder>();
+    private static BackOrderList backOrderList;
+
+    private BackOrderList() {
+
+    }
+
+    public static BackOrderList getInstance() {
+        if (backOrderList == null) {
+            backOrderList = new BackOrderList();
+        }
+        return backOrderList;
+    }
+
+    /**
+     * Check whether a back order with a given back order id exists.
+     * @param backOrderId String the id of the back order.
+     * @return BackOrder back order if found, null otherwise.
+     */
+    public BackOrder search(String backOrderId) {
+        for (Iterator<BackOrder> iterator = backOrders.iterator(); iterator.hasNext();) {
+            BackOrder backOrder = iterator.next();
+            if(backOrder.getId().equals(backOrderId)) {
+                return backOrder;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Inserts a back order into the collection.
+     * 
+     * @param backOrder BackOrder the back order to be inserted.
+     * @return boolean true if the back order could be inserted.
+     */
+    public boolean insertBackOrder(BackOrder backOrder) {
+        backOrders.add(backOrder);
+        return true;
+    }
 }
