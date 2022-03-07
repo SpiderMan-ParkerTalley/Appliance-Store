c2755558f152c165eef3131c09693e36a025352b
Jim Sawicki <jamesvsawicki@gmail.com>
2022-03-07 04:13:13 -0600

    initial ApplianceStore, Result

--- Project1ApplianceStore/edu.ics372.project1/edu/ics372/project1/appliancestore/business/facade/ApplianceStore.java
+++ Project1ApplianceStore/edu.ics372.project1/edu/ics372/project1/appliancestore/business/facade/ApplianceStore.java
@@ -1,5 +1,62 @@
 package edu.ics372.project1.appliancestore.business.facade;
 
-public class ApplianceStore {
+import java.io.Serializable;
 
+/**
+ * This is the facade class. It handles all requests from users.
+ * 
+ * @author Jim Sawicki and Sharon Shin
+ *
+ */
+public class ApplianceStore implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	private static ApplianceStore applianceStore;
+
+//	private static customers = CustomerList.instance();
+//	private static models = ModelList.instance();
+//	private static backOrders = BackOrderList.instance();
+
+	/**
+	 * The constructor is private in order to implement the singleton design
+	 * pattern.
+	 */
+	private ApplianceStore() {
+	}
+
+	/**
+	 * If no singleton has been created, creates a singleton. If a singleton already
+	 * exists, returns the singleton.
+	 * 
+	 * @return A singleton of type ApplianceStore.
+	 */
+	public ApplianceStore instance() {
+		if (applianceStore == null) {
+			return applianceStore = new ApplianceStore();
+		} else {
+			return applianceStore;
+		}
+	}
+
+	/**
+	 * This method allows for the purchase for a single appliance type for a single
+	 * customer of a variable amount per use case 4. If the quantity of the
+	 * appliance requested is not available, then the method processes a purchase of
+	 * whatever quantity of appliances is available and generates a backOrder object
+	 * of the unfulfilled quantity. This backOrder object is stored in the backOrder
+	 * list. Finally, the method returns a Result object with either an error code
+	 * or the appropriate fields to verify the purchase of the appliance and/or the
+	 * backOrder generated.
+	 * 
+	 * @param applianceId - Identifies the model and brand of appliance being
+	 *                    purchased.
+	 * @param CustomerId  - Identifies the customer purchasing the appliance.
+	 * @param quantity    - the amount of appliances being purchased.
+	 * @return A Result object with the appropriate information.
+	 */
+	public Result purchaseAppliance(String applianceId, String CustomerId, int quantity) {
+		Result result;
+
+		return result;
+	}
 }
--- Project1ApplianceStore/edu.ics372.project1/edu/ics372/project1/appliancestore/business/facade/Result.java
+++ Project1ApplianceStore/edu.ics372.project1/edu/ics372/project1/appliancestore/business/facade/Result.java
@@ -1,5 +1,28 @@
 package edu.ics372.project1.appliancestore.business.facade;
 
-public class Result {
+/**
+ * Ferries the results of operations in the facade to the UI.
+ * 
+ * @author Jim Sawicki and Sharon Shin
+ *
+ */
+public class Result extends DataTransfer {
+
+	private static final int APPLIANCE_NOT_FOUND = 1;
+	private static final int CUSTOMER_NOT_FOUND = 2;
+	private static final int BACK_ORDER_NOT_FOUND = 3;
+	private static final int OPERATION_SUCCESSFUL = 4;
+	private static final int OPERATION_FAILED = 5;
+
+	private int resultCode;
+
+	public int getResultCode() {
+		return resultCode;
+	}
+
+	public void setResultCode(int resultCode) {
+		this.resultCode = resultCode;
+	};
 
 }
+
