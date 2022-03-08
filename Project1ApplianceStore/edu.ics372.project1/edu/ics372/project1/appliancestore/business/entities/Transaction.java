cb352bc57f9a7e0643009030dd573afe484d6d79
Parker Talley <ptmn9278@gmail.com>
2022-03-08 14:39:49 -0600

    Restructed Getters

--- Project1ApplianceStore/edu.ics372.project1/edu/ics372/project1/appliancestore/business/entities/Transaction.java
+++ Project1ApplianceStore/edu.ics372.project1/edu/ics372/project1/appliancestore/business/entities/Transaction.java
@@ -3,11 +3,33 @@ package edu.ics372.project1.appliancestore.business.entities;
 public class Transaction {
     private static final String TRANSACTION_STRING = "TRANS";
     private static int idCounter = 1;
+
+    /**
+     * Stores reference to customer.
+     */
     private Customer customer;
+
+    /**
+     * Stores reference to appliance.
+     */
     private Appliance appliance;
+
+    /**
+     * Stores the number of appliance purchased.
+     */
     private int quantity;
+
+    /**
+     * Stores transaction code.
+     */
     private String code;
 
+    /**
+     * Constructor for transaction.
+     * @param customer Customer customer associated with transction.
+     * @param appliance Appliance appliance associated with transaction.
+     * @param quantity int quantity of appliance in transaction.
+     */
     public Transaction(Customer customer, Appliance appliance, int quantity) {
         this.setCustomer(customer);
         this.setAppliance(appliance);
@@ -15,35 +37,37 @@ public class Transaction {
         setCode(TRANSACTION_STRING + idCounter++);
     }
 
-    public String getCode() {
-        return code;
-    }
-
+    // Setters
     public void setCode(String code) {
         this.code = code;
-    }
-
-    public int getQuantity() {
-        return quantity;
     }
 
     public void setQuantity(int quantity) {
         this.quantity = quantity;
     }
 
-    public Appliance getAppliance() {
-        return appliance;
-    }
-
     public void setAppliance(Appliance appliance) {
         this.appliance = appliance;
     }
 
-    public Customer getCustomer() {
-        return customer;
-    }
-
     public void setCustomer(Customer customer) {
         this.customer = customer;
+    }
+
+    // Getters
+    public String getCode() {
+        return code;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+    
+    public Appliance getAppliance() {
+        return appliance;
+    }
+
+    public Customer getCustomer() {
+        return customer;
     }
 }
