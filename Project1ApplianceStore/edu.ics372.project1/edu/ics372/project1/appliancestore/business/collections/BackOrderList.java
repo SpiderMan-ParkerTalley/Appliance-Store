259b74b567f7e31693223e9bc14a4c09e6cac5e4
Jim Sawicki <jamesvsawicki@gmail.com>
2022-03-07 22:00:07 -0600

    change

--- Project1ApplianceStore/edu.ics372.project1/edu/ics372/project1/appliancestore/business/collections/BackOrderList.java
+++ Project1ApplianceStore/edu.ics372.project1/edu/ics372/project1/appliancestore/business/collections/BackOrderList.java
@@ -1,5 +1,15 @@
 package edu.ics372.project1.appliancestore.business.collections;
 
+import edu.ics372.project1.appliancestore.business.facade.Result;
+
 public class BackOrderList {
+
+    public static Result createBackOrder(String customerId, String applianceId, int backOrdersNeeded) {
+        return null;
+    }
     // Adding a secert comment
+
+    public static BackOrderList instance() {
+        return null;
+    }
 }
